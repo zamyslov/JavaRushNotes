cdd27034a9eeb2b9a0d54c1afccad1105e7a7b8c
OlegZamyslov <zamyslov@gmail.com>
2017-10-08 15:16:54 +0300

    08.10.2017 db+hibernate

---
+++ src/main/java/zamyslov/entity/Note.java
@@ -0,0 +1,71 @@
+package zamyslov.entity;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "notes")
+public class Note implements Serializable {
+
+    private static final long serialVersionUID = 35145776513L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    @Column(name = "text")
+    public String text;
+
+    @Column(name = "isDone", columnDefinition = "BIT")
+    public boolean isDone;
+
+    @Column(name = "createdDate")
+    public Timestamp createdDate;
+
+    public Note() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", isDone=" + isDone +
+                ", createdDate=" + createdDate +
+                '}';
+    }
+}
